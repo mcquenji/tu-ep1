745b6a403f9321f743bb5f896de3341579bbea92
mcquenji <60017181+mcquenji@users.noreply.github.com>
2025-11-13 21:12:06 +0100

    feat(Aufgabenblatt3): impl aufgabe 2

--- Aufgabenblatt3/src/Aufgabe2.java
+++ Aufgabenblatt3/src/Aufgabe2.java
@@ -4,12 +4,26 @@
 public class Aufgabe2 {
 
     private static void printEvenNumbersDescending(int end) {
-        //TODO: Implementieren Sie hier Ihre Lösung für die Methode
+        if (end <= 0) {
+            System.out.println(0);
+            return;
+        }
+
+        end = end % 2 == 0 ? end : end - 1;
+
+        System.out.print(end + " ");
+
+        printEvenNumbersDescending(end - 2);
     }
 
     private static int countEqualNeighbors(String text) {
-        //TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return -1; //Zeile kann geändert oder entfernt werden.
+        if (text.length() <= 1)
+            return 0;
+
+        final var firstChar = text.charAt(0);
+        final var nextChar = text.charAt(1);
+
+        return (firstChar == nextChar ? 1 : 0) + countEqualNeighbors(text.substring(1));
     }
 
     public static void main(String[] args) {
@@ -22,13 +36,12 @@ public class Aufgabe2 {
         System.out.println(countEqualNeighbors("Schifffahrt"));
         System.out.println();
 
-        //DIE NACHFOLGENDEN ZEILEN SIND ZUM TESTEN DER METHODE.
-        //**********************************************************************
+        // DIE NACHFOLGENDEN ZEILEN SIND ZUM TESTEN DER METHODE.
+        // **********************************************************************
         assert (countEqualNeighbors("AA") == 1);
         assert (countEqualNeighbors("ABBBCCDDDE") == 5);
         assert (countEqualNeighbors("matt") == 1);
         assert (countEqualNeighbors("Schifffahrt") == 2);
-        //**********************************************************************
+        // **********************************************************************
     }
 }
-
