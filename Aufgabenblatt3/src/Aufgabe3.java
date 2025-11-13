7e5de45d4be9b83568e82b22b4ce5f095c20cf13
mcquenji <60017181+mcquenji@users.noreply.github.com>
2025-11-13 21:33:55 +0100

    feat(Aufgabenblatt3): impl aufgabe 3

--- Aufgabenblatt3/src/Aufgabe3.java
+++ Aufgabenblatt3/src/Aufgabe3.java
@@ -4,8 +4,20 @@
 public class Aufgabe3 {
 
     private static String checkBrackets(String text) {
-        //TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+        if (text.length() <= 1)
+            return "";
+
+        final var firstCharIsBracket = text.charAt(0) == '[';
+
+        final var lastCharIsBracket = text.charAt(text.length() - 1) == ']';
+
+        if (firstCharIsBracket && lastCharIsBracket)
+            return text.substring(1, text.length() - 1);
+
+        final var startIdx = firstCharIsBracket ? 0 : 1;
+        final var stopIdx = text.length() - (lastCharIsBracket ? 0 : 1);
+
+        return checkBrackets(text.substring(startIdx, stopIdx));
     }
 
     public static void main(String[] args) {
@@ -15,13 +27,13 @@ public class Aufgabe3 {
         System.out.println(checkBrackets(""));
         System.out.println(checkBrackets("[a"));
         System.out.println(checkBrackets("]b["));
-        //DIE NACHFOLGENDEN ZEILEN SIND ZUM TESTEN DER METHODE.
-        //**********************************************************************
+        // DIE NACHFOLGENDEN ZEILEN SIND ZUM TESTEN DER METHODE.
+        // **********************************************************************
         assert (checkBrackets("HGT[CDE]HJH").equals("CDE"));
         assert (checkBrackets("aa[[b]").equals("[b"));
         assert (checkBrackets("abc").equals(""));
         assert (checkBrackets("[a").equals(""));
         assert (checkBrackets("]b[").equals(""));
-        //**********************************************************************
+        // **********************************************************************
     }
 }
